200751445d89fdc682b66392da34021d53bd0e28
jordan <jordantmunoz@gmail.com>
2017-10-02 21:42:09 -0700

    updates models and gemfile

--- db/migrate/20170928050939_create_photos.rb
+++ db/migrate/20170928050939_create_photos.rb
@@ -1,7 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration[5.1]
   def change
     create_table :photos do |t|
-      t.references :gallery, foreign_key: true
+    	t.string		 :title, null: false
+      # t.references :gallery, foreign_key: true
 
       t.timestamps
     end
