7777ff13b3c21c3229ebe24e5454b0e036f4907a
jordan <jordantmunoz@gmail.com>
2017-10-27 23:25:54 -0700

    makes multiple updates

--- db/migrate/20170930043651_create_galleries.rb
+++ db/migrate/20170930043651_create_galleries.rb
@@ -2,7 +2,7 @@ class CreateGalleries < ActiveRecord::Migration[5.1]
   def change
     create_table :galleries do |t|
     	t.string 			:name, null: false
-    	t.references  :user, foreign_key: true
+    	t.references  :user
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930043651) do
+ActiveRecord::Schema.define(version: 20171027052854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,11 @@ ActiveRecord::Schema.define(version: 20170930043651) do
 
   create_table "photos", force: :cascade do |t|
     t.string "title", null: false
+    t.bigint "gallery_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
+    t.index ["gallery_id"], name: "index_photos_on_gallery_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,5 +49,4 @@ ActiveRecord::Schema.define(version: 20170930043651) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "galleries", "users"
 end
